7be67fd36fa4b65adbc86780c40a8e608510012d
AShoYaMog <ivan.litvinenko.personal@gmail.com>
2022-08-03 17:22:10 +0300

    aded rainbow mod

--- js/app.js
+++ js/app.js
@@ -4,18 +4,31 @@ let grid = 0;
 let gridInput = document.querySelector('#gridSize');
 let colorPicer = document.querySelector('#colorInput');
 let colorPaletteConteiner = document.querySelector('.palett');
+let defoltColor = document.querySelector('.defoltColor');
 let paletteElement = document.querySelectorAll('.buttonSmall');
+let rainbowButton = document.querySelector('#rainbowButton');
 document.addEventListener('mousedown', () => {mouseDown = true});
 document.addEventListener('mouseup', () => {mouseDown = false});
 sketchConteiner.addEventListener('mouseleave', () => {mouseDown = false});
 sketchConteiner.addEventListener('dragstart', (e) => {e.preventDefault()});
 sketchConteiner.addEventListener('drop', (e) => {e.preventDefault()});
-gridInput.addEventListener('mouseup', () => {document.querySelector('.gridSize').textContent = `Grid size: ${gridInput.value}x${gridInput.value}`;});
+gridInput.addEventListener('input', () => {document.querySelector('.gridSize').textContent = `Grid size: ${gridInput.value}x${gridInput.value}`;});
+gridInput.addEventListener('mouseup', () => {gridControle(grid)});
 paletteElement.forEach(function(button) {
     button.addEventListener('mouseup', function (e) {
-    console.log(e.target.value)
     colorPicer.value = e.target.value
     })
+})
+rainbowButton.addEventListener('click', () => {
+    if (rainbowButton.value == 'off') {
+        rainbowButton.value = 'on';
+        rainbowButton.setAttribute('style', 'background-color: #ADD8E6;');
+        return
+    };
+    if (rainbowButton.value == 'on') {
+        rainbowButton.value = 'off';
+        rainbowButton.removeAttribute('style');
+    };
 })
 
 function gridControle() {
@@ -51,18 +64,27 @@ function palettAddColor () {
 }
 
 function palletRemoveColor () {
+    console.log(colorPaletteConteiner.lastChild)
+    if (colorPaletteConteiner.lastChild ===  defoltColor) return;
     colorPaletteConteiner.removeChild(colorPaletteConteiner.lastChild)
 }
 
 function clearPalette () {
-    colorPaletteConteiner.innerHTML = ''
+    colorPaletteConteiner.innerHTML = defoltColor.innerHTML
+}
+
+function randomColor() {
+    const COLOR_COMBINATION = 16777215;
+    let randomColor = Math.floor(Math.random()*COLOR_COMBINATION).toString(16);
+    return '#' + randomColor
 }
 
 function draw (e) {
-    if (e.type === 'mouseover' && !mouseDown) return;    
-    e.target.style.backgroundColor = colorPicer.value;       
+    if (e.type === 'mouseover' && !mouseDown) return;
+    if (rainbowButton.value === 'on')  return e.target.style.backgroundColor = randomColor();
+    e.target.style.backgroundColor = colorPicer.value; 
 }
 
 gridControle(grid)
-
-
+let a = 255;
+alert(a.toString(16))
